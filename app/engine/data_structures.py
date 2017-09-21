47273a41b919333f159960887affdc0f89cf0324
Andrew Ang <ang.kunanit@gmail.com>
2017-09-21 07:02:58 -0400

    Improve matrix data structure class

--- app/engine/data_structures.py
+++ app/engine/data_structures.py
@@ -1,17 +1,24 @@
 from django.db.models.query import QuerySet
+from django.db.models.base import ModelBase
 from django.db.models import Model
 import numpy as np
+from django.db import transaction
+from collections import namedtuple
 
+
+@transaction.atomic
 def multiple_update(qset, field, values):
-    new_qset = qset.filter().count()
     if qset.count()!=len(values):
         raise Exception('dimension mismatch')
-    with transaction.atomic():
-        for item, value in zip(qset, values):
-            item.update(*{field:value})
+    for item, value in zip(qset, values):
+        setattr(item, field, value)
+        item.save()
 
 
 class Vector(object):
+    """
+    Vector data structure that wraps a django model
+    """
     def __init__(self, qset):
         self.qset = qset
         # self.len = qset.count()
@@ -29,57 +36,55 @@ class Vector(object):
         """
         return Vector(self.qset.filter(**kwargs))
 
-    def update(self, list):
+    def update(self, new_values):
         """
         Runs multiple update to replace values with elements of given list
         """
         multiple_update(self.qset,'value',new_values)
 
-    # def add(self, obj, update=False):
-    #     if isinstance(obj,list):
-    #         self.add_list(obj, update)
-    #     if isinstance(obj,Vector):
-    #         self.add_vector(obj, update)
-
-    # def add_vector(self, vector, update):
-    #     """
-    #     element-wise vector addition
-    #     """
-    #     values = vector.qset.values_list('value')
-    #     self.add_list(values, update)
-
-    # def add_list(self, list_to_add, update):
-    #     """
-    #     element-wise vector addition, that takes as input list
-    #     """
-    #     new_values = [i+j for i,j in zip(self.values+list_to_add)]
-    #     if update:
-    #         multiple_update(self.qset,'value',new_values)
-    #     else:
-    #         return new_values
-            
-    # def add_scalar(self, vector, update):
-    #     values = vector.qset.values_list('value')
+    def length(self):
+        return self.qset.count()
 
 
 class Matrix(object):
-    def __init__(self,model):
-        self.model = model
-        fields = [f.name for f in model._meta.get_fields()][1:]
-        # assumption: row field is defined first, col field is defined second
-        self.row_field = fields[0]
-        self.col_field = fields[1]
+    """
+    Matrix data structure that wraps a django model
+    """
 
+    Axes = namedtuple('Axes',['row','col'])
+    Axis = namedtuple('axis',['name','model','index'])
+
+    def __init__(self,model,indices=None):
+        if isinstance(model, ModelBase):
+            self.model = model
+        else:
+            raise ValueError
+
+        # assumption: first field definied in model definition is row field, 
+        # col field is defined second
+        self.axes = Matrix.Axes._make([
+            Matrix.Axis(
+                name=field.name,
+                model=field.remote_field.model,
+                index=index,
+            ) for field, index in zip(
+                self.model._meta.get_fields()[1:3],
+                self._validate_indices(indices)
+            )
+        ])
+
+        # filter qset by any provided indices
+        self.qset = self._filtered_qset()
         # sorts matrix elements so that they are in C-like indexing order
-        self.qset = model.objects.order_by(self.row_field, self.col_field)
-
+        self.qset.order_by(self.axes.row.name, self.axes.col.name)
 
         # TODO some validation to see if expected number of elements are present
     
     def shape(self):
-        row_model = self.model._meta.get_field(self.row_field).remote_field.model
-        col_model = self.model._meta.get_field(self.col_field).remote_field.model
-        return (row_model.objects.count(), col_model.objects.count())
+        """
+        Return 2-tuple of dimensions
+        """
+        return tuple([a.index.count() if a.index else a.model.objects.count() for a in self.axes])
 
     # TODO values() method to get 2d nparray
     def values(self):
@@ -87,6 +92,38 @@ class Matrix(object):
         Returns 2d np array
         """
         return np.array(self.qset.values_list('value',flat=True)).reshape(self.shape())
+
+    def _validate_indices(self, indices):
+        """
+        Validate and prepare indices input argument to Matrix constructor
+        """
+        if indices:
+            # expect list/tuple
+            if not isinstance(indices, (list, tuple)):
+                raise ValueError
+            # expect size 2
+            if len(indices)!=2:
+                raise ValueError
+            # both indices should be QuerySets or None
+            if not all([isinstance(idx, QuerySet) for idx in indices if idx]):
+                raise ValueError
+        else:
+            indices = [None, None]
+        return indices
+
+    def _filtered_qset(self):
+        """
+        Set qset attribute, default to all object instances, 
+        or if there are index filters specified, apply index filters
+        """
+        qset = self.model.objects.all()
+        filters = {}
+        for axis in self.axes:
+            if axis.index:
+                filters[axis.name+"__in"]=axis.index
+        if filters:
+            qset = qset.filter(**filters)
+        return qset
 
     def __getitem__(self,indices):
         """
@@ -99,41 +136,34 @@ class Matrix(object):
             matrix[queryset,:]
             matrix[model_instance,]
         """
-
         if not isinstance(indices, tuple):
             raise ValueError
 
         # Case: Matrix[idx,] -> row vector
         if len(indices)==1:
-            row_idx = indices[0]
-            col_idx = None
+            indices = (indices[0],None)
 
-        elif len(indices)==2:
+        if len(indices)==2:
             # replace slice objects (:) in indices with Nones
-            row_idx = indices[0] if not isinstance(indices[0],slice) else None
-            col_idx = indices[1] if not isinstance(indices[1],slice) else None
-
+            indices = [idx if not isinstance(idx,slice) else None for idx in indices]
         else:
             raise ValueError
 
         # Case: Matrix[:,:]
-        if not row_idx or col_idx: 
+        if not any(indices): 
             return self
-
-        filters = {}
-
-        for field,idx in {self.row_field:row_idx, self.col_field:col_idx}.items():
-            print field,idx
-            if isinstance(idx,Model):
-                filters[field] = idx
-            elif isinstance(idx,QuerySet):
-                filters[field+"__in"] = idx
-            elif idx:
-                raise NotImplementedError
-
-        if any([isinstance(idx,QuerySet) for idx in [row_idx,col_idx]]):
-            return Matrix(self.model.objects.filter(**filters))
+        
+        if any([isinstance(idx,QuerySet) for idx in indices]):
+            return Matrix(self.model, indices=indices)
         else:
+            filters = {}
+            for a,idx in zip(self.axes,indices):
+                if isinstance(idx,Model):
+                    filters[a.name] = idx
+                elif isinstance(idx,QuerySet):
+                    filters[a.name+"__in"] = idx
+                elif idx:
+                    raise NotImplementedError
             return Vector(self.model.objects.filter(**filters))
 
 
