66d9edcc3f83d0ba75ca682dd8fdedcb1c3e6efb
Andrew Ang <ang.kunanit@gmail.com>
2018-02-14 23:09:04 -0500

    Update local docker config

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -12,7 +12,11 @@ INSTALLED_APPS += [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': 5432,
     }
 }
