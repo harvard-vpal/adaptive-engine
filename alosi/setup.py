a702e32beafd4cd1c781a1da51b4887cca0c0dd3
Andrew Ang <ang.kunanit@gmail.com>
2018-05-09 17:03:03 -0400

    Rename python package to alosi

--- alosi/setup.py
+++ alosi/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='alosi_engine',
+    name='alosi',
     version='0.0.1',
     description='Adaptive learning engine',
     url='https://github.com/harvard-vpal/adaptive-engine',
     author='Andrew Ang',
     author_email='andrew_ang@harvard.edu',
     license='Apache-2.0',
-    packages=['alosi_engine'],
+    packages=['alosi'],
     install_requires=[
         'numpy',
     ],
