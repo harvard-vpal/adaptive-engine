a94ac9a9c76a6f43a43cf42ce60b85ad07a7093d
Andrew Ang <ang.kunanit@gmail.com>
2018-07-09 16:45:33 -0400

    Update docstrings for future v2 changes

--- app/engine/api_v2.py
+++ app/engine/api_v2.py
@@ -8,24 +8,39 @@ from .engines import get_engine
 
 
 class ActivityViewSet(viewsets.ModelViewSet):
+    """
+    Activity-related API endpoints
+
+    Standard CRUD endpoints:
+        GET /activity - list
+        POST /activity - create
+        GET /activity/{id} - retrieve
+        PUT /activity/{id} - update
+        PATCH /activity/{id} - partial update
+        DELETE /activity/{id} - destroy
+
+    Additional endpoints:
+        POST /activity/recommend - recommend activity
+    """
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
 
     @action(methods=['post'], detail=False)
     def recommend(self, request):
         """
-        API endpoint: Recommend activity
-        /engine/api/activity/recommend
-        Body:
-            learner: {
-                'tool_consumer_instance_guid': str,
-                'user_id': str
-            }
-            collection: {
-                'collection_id': str
-            }
-            sequence: (optional) json of activities that exist in sequence
-                [
+        Recommends an activity
+
+        POST /activity/recommend
+        Request Body:
+            {
+                learner: {
+                    'tool_consumer_instance_guid': str,
+                    'user_id': str
+                }
+                collection: {
+                    'collection_id': str
+                }
+                sequence: [
                     {
                         activity: <source_launch_url>,
                         score: <score>,
@@ -33,6 +48,7 @@ class ActivityViewSet(viewsets.ModelViewSet):
                     },
                     ...
                 ]
+            }
         """
         # validate request serializer
         serializer = ActivityRecommendationRequestSerializer(
@@ -66,3 +82,110 @@ class ActivityViewSet(viewsets.ModelViewSet):
             )
 
         return Response(recommendation_data)
+
+
+class CollectionViewSet(viewsets.ModelViewSet):
+    """
+    Collection-related API endpoints
+
+    Standard CRUD endpoints:
+        GET /collection - list
+        POST /collection - create
+        GET /collection/{slug} - retrieve
+        PUT /collection/{slug} - update
+        PATCH /collection/{slug} - partial update
+        DELETE /collection/{slug} - destroy
+
+    Additional endpoints:
+        GET /collection/{slug}/activities - list activities in collection
+        POST /collection/{slug}/activities - modify activities in collection
+        POST /collection/grade - get collection grade for a learner
+    """
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+
+    @action(methods=['get', 'post'], detail=True)
+    def activities(self, request, pk=None):
+        """
+        Handles two API endpoints:
+
+        1. Get activities in collection
+            GET /<collection_id>
+
+        2. Update activities in collection
+            POST /<collection_id>
+
+            Request body: list of activities that exist in collection
+                [
+                    {
+                        source_launch_url: <https://example.com/1>,
+                        name: <str>,
+                        tags: <str>,
+                        type: <str>,
+                        difficulty: <float>
+                    },
+                    {
+                        source_launch_url: <https://example.com/2>,
+                        name: <str>,
+                        tags: <str>,
+                        type: <str>,
+                        difficulty: <float>
+                    },
+                    ...
+                ]
+
+            Update behavior:
+            - Any activities that do not already exist (based on source_launch_url) will be created,
+            and will belong to the relevant collection.
+            - Any activities that already exist in the collection will have their fields updated based on the new data.
+            - Any activities that existed in the collection previously but are not included in the new request data will
+            be removed from the collection (but not deleted from the engine).
+        """
+        # TODO create collection based on string id
+        collection, created = Collection.objects.get_or_create(pk=pk)
+        activities = collection.activity_set.all()
+        if request.method == 'POST':
+            serializer = CollectionActivitySerializer(
+                activities,
+                data=request.data,
+                many=True,
+                context={'collection': collection}
+            )
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            serializer = CollectionActivitySerializer(
+                activities,
+                many=True,
+                context={'collection': collection}
+            )
+        return Response(serializer.data)
+
+    @action(methods=['post'], detail=True)
+    def grade(self, request):
+        """
+        Returns a grade between 0.0 and 1.0 for the specified learner and collection.
+
+        POST /collection/grade
+        Request Body:
+            {
+                learner: {
+                    'tool_consumer_instance_guid': str,
+                    'user_id': str
+                }
+                collection: {
+                    'collection_id': str
+                }
+            }
+        """
+        collection = self.get_object()
+        try:
+            # TODO this should be a serializer
+            learner_id = int(request.data.get('learner', None))
+        except:
+            msg = "Learner id not provided, or not valid"
+            return Response({'message': msg}, status=status.HTTP_400_BAD_REQUEST)
+        grade = collection.grade(learner_id)
+        return Response({'grade': grade, 'learner': learner_id})
