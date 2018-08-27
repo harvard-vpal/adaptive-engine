ae05cb179d630bea611d9a5402527a6e8e009ba7
Andrew Ang <ang.kunanit@gmail.com>
2018-08-27 19:06:36 -0400

    Add id field to prereq activity serializer just in case

--- app/engine/serializers.py
+++ app/engine/serializers.py
@@ -289,6 +289,6 @@ class PrerequisiteActivitySerializer(serializers.ModelSerializer):
         model = Activity.prerequisite_activities.through
         # from_activity: dependent activity
         # to_activity: prerequisite activity
-        fields = ('from_activity','to_activity')
+        fields = ('id','from_activity','to_activity')
 
 # Activity.prerequisite_activities.through.
