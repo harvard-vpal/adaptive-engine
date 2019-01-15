f122e59747b840caad54904a07ad8b60aedc6f51
Andrew Ang <ang.kunanit@gmail.com>
2019-01-15 13:28:57 -0500

    fix: install django_filters in proper settings module

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters',
     'engine',
 ]
 
--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -4,7 +4,6 @@ SECRET_KEY = 'sp(j(ts6ri()muwz-$^i+k+jgjfv$jbgs@9oq@lzy6x5@lynqd'
 
 INSTALLED_APPS += [
     'django_extensions',
-    'django_filters',
 ]
 
 ALLOWED_HOSTS += ['localhost', 'engine']
